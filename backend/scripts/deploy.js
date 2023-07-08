863f40fac190e0eaec6491cfaf84074de8e8732f
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2023-07-08 11:04:11 +0400

    init

---
+++ backend/scripts/deploy.js
@@ -0,0 +1,9 @@
+const hre = require("hardhat");
+const fs = require("fs/promises");
+
+async function main() {}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ frontend/src/CreateVotes.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateVotes = () => {
+  return <div></div>;
+};
+
+export default CreateVotes;
