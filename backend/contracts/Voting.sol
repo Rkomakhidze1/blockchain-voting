58a339f7bdcfed7eebac638aae8e8f2c846903af
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2023-07-09 11:14:11 +0400

    added structs and mappings for voting contract

--- backend/contracts/Voting.sol
+++ backend/contracts/Voting.sol
@@ -2,5 +2,16 @@
 pragma solidity ^0.8.9;
 
 contract Voting {
-   
+    uint256 nextVoteId;
+
+    struct Vote {
+        string uri;
+        address owner;
+        uint256 endTime;
+        uint256[] votes;
+        mapping(address => bool) voted;
+        uint256 options;
+    }
+    mapping(uint256 => Vote) votes;
+    mapping(address => bool) public members;
 }
