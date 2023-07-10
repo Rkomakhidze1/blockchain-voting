84135d522626e902b33707feaf9c587fc26246bb
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2023-07-10 12:14:16 +0400

    added events and modifiers for voting

--- backend/contracts/Voting.sol
+++ backend/contracts/Voting.sol
@@ -14,4 +14,31 @@ contract Voting {
     }
     mapping(uint256 => Vote) votes;
     mapping(address => bool) public members;
+
+    event MemberJoined(address indexed member, uint256 joinedAt);
+    event VoteCreated(
+        address indexed owner,
+        uint256 indexed voteId,
+        uint256 indexed createdAt,
+        uint256 endTime
+    );
+    event Voted(
+        address indexed voter,
+        uint256 indexed voteId,
+        uint256 indexed option,
+        uint256 createdAt
+    );
+
+    modifier isMember() {
+        require(members[msg.sender], "you are not a member");
+        _;
+    }
+
+    modifier canVote(uint256 voteId, uint256 option) {
+        require(voteId < nextVoteId, "vote does not exist");
+        require(option < votes[voteId].options, "invalid option");
+        require(!votes[voteId].voted[msg.sender], "you have already voted");
+        require(block.timestamp <= votes[voteId].endTime, "vote has ended");
+        _;
+    }
 }
