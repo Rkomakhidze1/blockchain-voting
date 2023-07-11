a806f22099e08b4f628d6bde2b07ace49d391037
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2023-07-11 15:52:10 +0400

    added tests for joining

--- backend/test/Voting.js
+++ backend/test/Voting.js
@@ -1,1 +1,29 @@
-describe("Voting", function () {});
+const { expect } = require("chai");
+
+const getTime = async () => {
+  const blockNumBefore = await ethers.provider.getBlockNumber();
+  const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+  return blockBefore.timestamp;
+};
+
+describe("Voting", function () {
+  let addr0;
+  let addr1;
+  let voting;
+
+  before(async () => {
+    [addr0, addr1] = await ethers.getSigners();
+
+    const Voting = await ethers.getContractFactory("Voting");
+    voting = await Voting.deploy({});
+  });
+
+  describe("Join", () => {
+    it("Can join", async () => {
+      await expect(voting.join()).to.emit(voting, "MemberJoined");
+    });
+    it("Cannot join if already member", async () => {
+      await expect(voting.join()).to.be.reverted;
+    });
+  });
+});
