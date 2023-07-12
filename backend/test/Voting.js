f3586b30674070cef69c718d3ea83cf406e02d7f
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2023-07-12 15:52:43 +0400

    added tests for creating votes

--- backend/test/Voting.js
+++ backend/test/Voting.js
@@ -26,4 +26,36 @@ describe("Voting", function () {
       await expect(voting.join()).to.be.reverted;
     });
   });
+
+  describe("Create vote", () => {
+    it("Cannot create vote if not member", async () => {
+      await expect(
+        voting.connect(addr1).createVote("", (await getTime()) + 60, 4)
+      ).to.be.reverted;
+    });
+    it("Cannot create vote invalid end time", async () => {
+      await expect(voting.createVote("", (await getTime()) - 60, 4)).to.be
+        .reverted;
+    });
+    it("Cannot create vote it too few options", async () => {
+      await expect(voting.createVote("", (await getTime()) + 60, 1)).to.be
+        .reverted;
+    });
+    it("Cannot create vote if too many options", async () => {
+      await expect(voting.createVote("", (await getTime()) + 60, 9)).to.be
+        .reverted;
+    });
+    it("Can create vote", async () => {
+      await expect(voting.createVote("1", (await getTime()) + 60, 3)).to.emit(
+        voting,
+        "VoteCreated"
+      );
+    });
+    it("Can create 2nd vote", async () => {
+      await expect(voting.createVote("2", (await getTime()) + 60, 4)).to.emit(
+        voting,
+        "VoteCreated"
+      );
+    });
+  });
 });
