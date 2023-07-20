0713727aab847ccead73fb510e589319bd64f4d8
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2023-07-20 11:41:31 +0400

    adde app structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import CreateVotes from "./CreateVotes";
+import Votes from "./Votes";
 
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="create-vote" element={<CreateVotes />} />
+          <Route path="votes" element={<Votes />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
