b2c3da21e620d693ead0364d7e1f593f5c57e140
Rezo Komakhidze <rezokomakhidze@Rezos-MacBook-Pro.local>
2023-07-22 17:33:54 +0400

    added structs and mappings

--- backend/contracts/Voting.sol
+++ backend/contracts/Voting.sol
@@ -2,6 +2,19 @@
 pragma solidity ^0.8.9;
 
 contract Voting {
+    uint256 nextVoteId;
+
+    struct Vote {
+        string uri;
+        address owner;
+        uint256 endTime;
+        uint256[] votes;
+        mapping(address => bool) voted;
+        uint256 options;
+    }
+    mapping(uint256 => Vote) votes;
+    mapping(address => bool) public members;
+
     event MemberJoined(address indexed member, uint256 joinedAt);
     event VoteCreated(
         address indexed owner,
